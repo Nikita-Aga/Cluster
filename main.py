7b475dc8a93906478a36cd79ec874c0e33de034a
Nikita-Aga <74054270+Nikita-Aga@users.noreply.github.com>
2023-12-27 23:35:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import os
+import os,subprocess
 os.system("pip install undetected-chromedriver==3.5.3")
 os.system("pip install discord.py==2.1.0")
+import requests
+result = subprocess.run("cloudflared tunnel --url http://localhost:3333 --no-autoupdate",shell=True,stdout=subprocess.PIPE)
+print(result.stdout)
