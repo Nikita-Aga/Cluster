c75b01a3830662e69777c3ce61b3d9bd0bcda630
Nikita-Aga <74054270+Nikita-Aga@users.noreply.github.com>
2023-12-28 00:06:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import os,subprocess
 os.system("pip install undetected-chromedriver==3.5.3")
 os.system("pip install discord.py==2.1.0")
 import requests
-os.system("tmux new -t aa")
-os.system("cloudflared tunnel --url http://localhost:3333 --no-autoupdate")
+import threading
+import time
+def cloud_flared():
+  os.system("cloudflared tunnel --url http://localhost:3333 --no-autoupdate")
+
+th = threading.Thread(target=cloud_flared)
+th.start()
+time.sleep(10)
 
 
 print(111)
