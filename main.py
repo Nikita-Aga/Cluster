1954a1515a7c8bc2945f85d4796ae2025acacf5c
Nikita-Aga <74054270+Nikita-Aga@users.noreply.github.com>
2023-12-28 17:01:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ os.system("pip install discord.py==2.1.0")
 import requests
 import threading
 import time
+from subprocess import Popen, PIPE
 def cloud_flared():
+  run = True
+  with Popen([sys.executable, '-u', 'child.py'],
+             stdout=PIPE, universal_newlines=True) as process:
+      for line in process.stdout:
+          print(line.replace('!', '#'), end='')
   os.system("cloudflared tunnel --url http://localhost:3333 --no-autoupdate")
 
 th = threading.Thread(target=cloud_flared)
