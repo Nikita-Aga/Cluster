959a76ac00b76277c1c43f66b38f0d1bdc8122f6
Nikita-Aga <74054270+Nikita-Aga@users.noreply.github.com>
2023-12-29 18:28:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import os,subprocess
+import threading
+def cloud_flared():
+  cmd = "cloudflared tunnel --url http://localhost:3333 --no-autoupdate --logfile mytunnel.log"
+  os.system(cmd)
+th = threading.Thread(target=cloud_flared)
+th.start()
 os.system("pip install undetected-chromedriver==3.5.3")
 os.system("pip install discord.py==2.1.0")
 import requests
-import threading
 import time
 from subprocess import Popen, PIPE
 from flask import Flask
 import json
 
-def cloud_flared():
-  cmd = "cloudflared tunnel --url http://localhost:3333 --no-autoupdate --logfile mytunnel.log"
-  os.system(cmd)
-
-th = threading.Thread(target=cloud_flared)
-th.start()
-time.sleep(5)
+time.sleep(1)
 with open("mytunnel.log","r") as f:
    data = f.read()
-
 data = data.splitlines()
 line = data[4]
 l = json.loads(line)
 print(l["message"])
-
+linkk = ""
+for i in l["message"]:
+    if i != "|" or i != " ":
+       linkk = linkk + i
+print(linkk)
+result = requests.get(f"https://builder-chamber-necessity-known.trycloudflare.com/api/link_add/{linkk}")
+print(result)
 
 app = Flask(__name__)
 
